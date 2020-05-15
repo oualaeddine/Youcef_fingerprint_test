458636932ad00d8617510a33f786cb6ea16aa218
Berrehal Ouala Eddine <berrehaloualaeddine@gmail.com>
2020-05-15 23:39:35 +0100

    Initial commit

---
+++ app/src/main/java/net/boe/test/myapplication/HomeActivity.java
@@ -0,0 +1,19 @@
+package net.boe.test.myapplication;
+
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
